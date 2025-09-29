1529b4a84a7e6dc031a1aa9db396da768b0f92ef
fyraschatali1122-png <fyraschatali1122@gmail.com>
2025-09-30 01:35:25 +0200

    config.js

--- config.js
+++ config.js
@@ -2,6 +2,7 @@
 // Dans Google Sheets : Fichier → Partager → Publier sur le Web → Feuille → CSV → Publier → copier le lien
 // Exemple: const CSV_URL = "https://docs.google.com/spreadsheets/d/....../pub?output=csv";
 const CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTOuHwb0HGELAZhFI3d4vrmWxevqbwwzPgTIOD8TIGII_x7oSsJleuGLdX4cYb5V373IX_Rn1RPrh_Q/pub?gid=569786249&single=true&output=csv";
+const WEBAPP_URL = "https://script.google.com/macros/s/AKfycbzjLpFTbPsrye8ZbxS0Wiqgyusw9FWYcVk3hM6buVBnuTMsBR36pTfyXurRFUK1I0iK2w/exec";
 
 // Option: fuseau pour afficher les dates/heures si besoin de décalage (laisse "" sinon)
 const TIMEZONE = ""; // ex: "Europe/Berlin"
