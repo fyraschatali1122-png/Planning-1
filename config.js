36641c078681ac02fa23d7f965ddb4b2c7de6a1e
fyraschatali1122-png <fyraschatali1122@gmail.com>
2025-09-30 02:40:42 +0200

    config.js

--- config.js
+++ config.js
@@ -1,8 +1,6 @@
-// ⚙️ Configure ici l'URL CSV publiée de ton Google Sheet (ou n'importe quel CSV accessible publiquement).
-// Dans Google Sheets : Fichier → Partager → Publier sur le Web → Feuille → CSV → Publier → copier le lien
-// Exemple: const CSV_URL = "https://docs.google.com/spreadsheets/d/....../pub?output=csv";
-const CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTOuHwb0HGELAZhFI3d4vrmWxevqbwwzPgTIOD8TIGII_x7oSsJleuGLdX4cYb5V373IX_Rn1RPrh_Q/pub?gid=569786249&single=true&output=csv";
-const WEBAPP_URL = "https://script.google.com/macros/s/AKfycbzjLpFTbPsrye8ZbxS0Wiqgyusw9FWYcVk3hM6buVBnuTMsBR36pTfyXurRFUK1I0iK2w/exec";
+// config.js
 
-// Option: fuseau pour afficher les dates/heures si besoin de décalage (laisse "" sinon)
-const TIMEZONE = ""; // ex: "Europe/Berlin"
+const CONFIG = {
+  CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTOuHwb0HGELAZhFI3d4vrmWxevqbwwzPgTIOD8TIGII_x7oSsJleuGLdX4cYb5V373IX_Rn1RPrh_Q/pub?gid=569786249&single=true&output=csv",
+  WEBAPP_URL: "https://script.google.com/macros/s/AKfycbzjLPfTbPsrye8ZbxS0Wiqqysuw9FWYcVk3hfYFQ/exec"
+};
